07c3fde3a1a94ff10f77467144461f40fa00be96
Simon Stelling <stellingsimon@gmail.com>
2017-07-09 12:15:21 +0200

    fix maven groupId

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject whatsoup "0.1.0-SNAPSHOT"
+(defproject stellingsimon/whatsoup "0.1.0-SNAPSHOT"
   :description "A soup generator"
   :url "https://github.com/stellingsimon/whatsoup"
   :license {:name "Eclipse Public License"
