47c5fffcd578d8919f098738be39b1b53ace2a91
Simon Stelling <stellingsimon@gmail.com>
2017-07-23 10:38:51 +0200

    add tests for `food-kb/diversity-score`

--- test/whatsoup/food_kb_test.clj
+++ test/whatsoup/food_kb_test.clj
@@ -30,3 +30,28 @@
   (is (almost= 0.0 (score {} :food/broccoli #{:food/broccoli})))
   (is (almost= 0.5 (score test-kb :food/broccoli #{:food/lauch})))
   (is (almost= 5.0 (score test-kb :food/schweinefleisch #{:food/lauch}))))
+
+(deftest -diversity-score
+  (let [kb
+        (component/start
+          (->FoodKnowledgeBase
+            {:food-catalog              [{:food/name       :food/a
+                                          :food/properties #{:property/one :property/two :property/three :property/all}}
+                                         {:food/name       :food/b
+                                          :food/properties #{:property/two :property/three :property/all}}
+                                         {:food/name       :food/c
+                                          :food/properties #{:property/three :property/all}}
+                                         {:food/name  :food/d
+                                          :food/properties #{:property/de-only :property/d-only :property/all}}
+                                         {:food/name  :food/e
+                                          :food/properties #{:property/de-only :property/e-only :property/all}}
+                                         {:food/name  :food/f
+                                          :food/properties #{:property/fgh-only :property/f-only :property/all}}
+                                         {:food/name  :food/g
+                                          :food/properties #{:property/fgh-only :property/g-only :property/all}}
+                                         {:food/name  :food/h
+                                          :food/properties #{:property/fgh-only :property/h-only :property/all}}]
+             :food-compatibility-matrix {}}))]
+    (is (= (diversity-score kb :food/a :food/b) (diversity-score kb :food/b :food/a)) "commutativity")
+    (is (< (diversity-score kb :food/a :food/b) (diversity-score kb :food/b :food/c)) "smaller overlap -> higher score")
+    (is (< (diversity-score kb :food/d :food/e) (diversity-score kb :food/f :food/g)) "overlap in less specific group -> higher score")))
